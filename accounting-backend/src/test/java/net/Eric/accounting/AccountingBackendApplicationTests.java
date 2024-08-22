d7879a5694709964140fb8e2d96ae0e54b7f5a11
Ansathsean <eric267213@hotmail.com>
2024-08-22 22:40:13 +0800

    第一次上版

---
+++ accounting-backend/src/test/java/net/Eric/accounting/AccountingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.Eric.accounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
