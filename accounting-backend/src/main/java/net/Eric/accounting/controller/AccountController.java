d8827153f01756f6436613eb90c7ea2074047d93
Ansathsean <eric267213@hotmail.com>
2024-09-03 22:36:26 +0800

    新增Add category

--- accounting-backend/src/main/java/net/Eric/accounting/controller/AccountController.java
+++ accounting-backend/src/main/java/net/Eric/accounting/controller/AccountController.java
@@ -4,6 +4,7 @@ package net.Eric.accounting.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import net.Eric.accounting.dto.AccountDto;
 import net.Eric.accounting.service.AccountsService;
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/accounts")
 public class AccountController {
